ccfd24aace206d0e0a20106858deb7748c9d7146
Csengeri Attila <cseattila@gmail.com>
2021-12-28 22:02:00 +0100

    ezt tatláltam

--- single.php
+++ single.php
@@ -14,8 +14,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="single-wrapper">
 
-	<!--  div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1" -->
-
 		<div class="row">
 
 			<!-- Do the left sidebar check -->
@@ -43,7 +41,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		</div><!-- .row -->
 
-	<!-- /div><!-- #content -->
+
 
 </div><!-- #single-wrapper -->
 
