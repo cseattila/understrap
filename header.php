28ba43edd4b6ca4044f5e0606680f25d7ac28c72
Csengeri Attila <cseattila@gmail.com>
2022-03-25 21:53:35 +0100

    részletek gomb

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
   <script type="text/javascript">
 	function  afterloadMenu(){
   	let dropdowns = document.querySelectorAll('.dropdown-toggle2')
