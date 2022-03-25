ef24ab456557ff5ca69ff23aea3d8fdc224e9744
Csengeri Attila <cseattila@gmail.com>
2022-03-25 20:42:41 +0100

    https

--- header.php
+++ header.php
@@ -33,7 +33,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 })
 }
   </script>
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>
 </head>
 
