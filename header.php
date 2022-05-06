619346eedf6c22f3d78060d67f59accad892ea55
Csengeri Attila <cseattila@gmail.com>
2022-05-06 21:27:08 +0200

    facebook icon

--- header.php
+++ header.php
@@ -95,8 +95,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 				
 				 </div>
 					 <div class="menu_after_2_soros" id="menu_after_2_soros" > &nbsp; </div>
-               <div class="menu also sor"  >
-
+               <div class="menu also sor" style=" display: table;     table-layout: fixed; /*Optional*/    border-spacing: 10px; /*Optional*/" >
+	           <div style=" display: table-cell;">
 				<!-- The WordPress Menu goes here -->
 				<?php
 				wp_nav_menu(
@@ -112,7 +112,18 @@ $container = get_theme_mod( 'understrap_container_type' );
 					)
 				);
 				?>
-				
+			</div>
+			  <div class="facebook-icon" style=" width:25px; heigth:25px; display: table-cell;color: #1778f2; "><a
+			  style ="   color: currentColor;  fill: currentColor;" 
+			  href="https://www.facebook.com/profile.php?id=100072298874419" > 
+			  <div style="background-color : white; border-radius: 5px; " >
+			  <svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false">
+			      <path d="M12 2C6.5 2 2 6.5 2 12c0 5 3.7 9.1 8.4 9.9v-7H7.9V12h2.5V9.8c0-2.5 1.5-3.9 3.8-3.9 1.1 0 2.2.2 2.2.2v2.5h-1.3c-1.2 0-1.6.8-1.6 1.6V12h2.8l-.4 2.9h-2.3v7C18.3 21.1 22 17 22 12c0-5.5-4.5-10-10-10z">
+			      </path>
+			  </svg>
+			  </div>
+			  </a> </div>
+	         
 				     </div>
 		</div><!-- class="menu_2_soros" -->
 		
